0a22acdae8a23d23cb06314ea90fc33d48881bf0
junaidasif-dev <junaidasifdev@gmail.com>
2024-04-21 18:29:10 +0500

    Complete Project

---
+++ FinalProject/README.md
@@ -0,0 +1,16 @@
+Project Documentation: FoodPanda Database Design and SQL's Queries Practice
+
+Introduction:
+This project aims to design and implement a comprehensive database system for managing food delivery services. This documentation outlines the structure, design decisions, and implementation details of the FoodPanda database. Additionally, it highlights the SQL practice undertaken during the project, including participation in HackerRank challenges resulting in the acquisition of Basic and Intermediate certificates.
+
+ERD Design:
+The Entity-Relationship Diagram (ERD) serves as the foundation for the FoodPanda database design. It visually represents the entities, their attributes, and the relationships between them. 
+
+MySQL Database Implementation:
+The FoodPanda database is implemented using MySQL Data Definition Language (DDL) commands. These commands define the structure of the database, including tables, columns, keys, and constraints.
+
+SQL Practice and HackerRank Certificates:
+During the development of this project, SQL practice sessions were conducted using HackerRank challenges. These challenges provided opportunities to enhance SQL skills and solve real-world problems. I successfully completed both Basic and Intermediate challenges, earning certificates to validate their proficiency.
+
+Conclusion:
+This project demonstrates the application of database design principles and SQL skills in developing a comprehensive food delivery database system. By creating an ERD, implementing the database using MySQL commands, and participating in SQL practice sessions on HackerRank, I have gained valuable experience and proficiency in database management and SQL querying. The FoodPanda database is poised to facilitate efficient food delivery services, benefiting both customers and restaurants.
--- FinalProject/practice.sql
+++ FinalProject/practice.sql
@@ -1,69 +1,3 @@
--- I used NorthWind Database for practicing complex queries of SQL
-
-USE northwind;
-
-SELECT * FROM Products;
-
--- Retrieve the product name, unit price, and units in stock of all products that cost less than $100, ordered by product name
-
-SELECT ProductName, UnitPrice, UnItsinStock
-FROM Products
-WHERE UnitPrice < 100
-ORDER BY ProductName;
-
-SELECT * FROM Orders;
-
--- Retrieve the order ID, customer ID, and order date of all orders that do not have a customer ID, ordered by order date
-
-SELECT OrderID, CustomerID, OrderDate
-FROM Orders
-WHERE CustomerID IS NULL
-ORDER BY OrderDate;
-
-
--- Write a query to show a list of customers and their corresponding orders.
-
--- Here we use inner join
-
-SELECT C.CompanyName, P.ProductName
-FROM Customers C
-JOIN Orders O ON C.CustomerID = O.CustomerID 
-JOIN [Order Details] OD ON O.OrderID = OD.OrderID
-JOIN Products P ON OD.ProductID = P.ProductID;
-
--- Write a query to show a list of employees and their corresponding managers.
-
--- Here we use self join.
-
-SELECT E.FirstName AS emp_name, M.FirstName AS manger_name
-FROM Employees E
-LEFT JOIN Employees M ON E.ReportsTo = M.EmployeeID;
-
-
--- Write a query to show a list of customers who have not placed any orders.
-
--- Here we use left anti join.
-
-SELECT C.CompanyName, P.ProductName
-FROM Customers C
-LEFT JOIN Orders O ON C.CustomerID = O.CustomerID 
-LEFT JOIN [Order Details] OD ON O.OrderID = OD.OrderID
-LEFT JOIN Products P ON OD.ProductID = P.ProductID
-WHERE P.ProductID IS NULL;
-
--- Write a query to show a list of employees and their corresponding territories, including those employees who have no territories assigned.
-
-SELECT * FROM Territories;
-SELECT * FROM Employees;
-SELECT * FROM EmployeeTerritories;
-
-SELECT E.FirstName, T.TerritoryDescription
-FROM Employees E
-LEFT JOIN EmployeeTerritories ET ON E.EmployeeID = ET.EmployeeID
-LEFT JOIN Territories T ON ET.TerritoryID = T.TerritoryID;
-
-
-
 -- HackerRank Queries
 
 -- Query all columns for all American cities in the CITY table with populations larger than 100000. The CountryCode for America is USA.
@@ -264,5 +198,71 @@ SELECT concat('There are a total of ', count(Name), ' ', lower(Occupation), 's.'
 FROM OCCUPATIONS
 GROUP BY Occupation
 ORDER BY COUNT(Occupation);
+
+
+
+-- I used NorthWind Database for practicing complex queries of SQL
+
+USE northwind;
+
+SELECT * FROM Products;
+
+-- Retrieve the product name, unit price, and units in stock of all products that cost less than $100, ordered by product name
+
+SELECT ProductName, UnitPrice, UnItsinStock
+FROM Products
+WHERE UnitPrice < 100
+ORDER BY ProductName;
+
+SELECT * FROM Orders;
+
+-- Retrieve the order ID, customer ID, and order date of all orders that do not have a customer ID, ordered by order date
+
+SELECT OrderID, CustomerID, OrderDate
+FROM Orders
+WHERE CustomerID IS NULL
+ORDER BY OrderDate;
+
+
+-- Write a query to show a list of customers and their corresponding orders.
+
+-- Here we use inner join
+
+SELECT C.CompanyName, P.ProductName
+FROM Customers C
+JOIN Orders O ON C.CustomerID = O.CustomerID 
+JOIN [Order Details] OD ON O.OrderID = OD.OrderID
+JOIN Products P ON OD.ProductID = P.ProductID;
+
+-- Write a query to show a list of employees and their corresponding managers.
+
+-- Here we use self join.
+
+SELECT E.FirstName AS emp_name, M.FirstName AS manger_name
+FROM Employees E
+LEFT JOIN Employees M ON E.ReportsTo = M.EmployeeID;
+
+
+-- Write a query to show a list of customers who have not placed any orders.
+
+-- Here we use left anti join.
+
+SELECT C.CompanyName, P.ProductName
+FROM Customers C
+LEFT JOIN Orders O ON C.CustomerID = O.CustomerID 
+LEFT JOIN [Order Details] OD ON O.OrderID = OD.OrderID
+LEFT JOIN Products P ON OD.ProductID = P.ProductID
+WHERE P.ProductID IS NULL;
+
+-- Write a query to show a list of employees and their corresponding territories, including those employees who have no territories assigned.
+
+SELECT * FROM Territories;
+SELECT * FROM Employees;
+SELECT * FROM EmployeeTerritories;
+
+SELECT E.FirstName, T.TerritoryDescription
+FROM Employees E
+LEFT JOIN EmployeeTerritories ET ON E.EmployeeID = ET.EmployeeID
+LEFT JOIN Territories T ON ET.TerritoryID = T.TerritoryID;
 
 
